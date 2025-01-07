e3e82bfe2c83706a6d494a186ddbf2dec4a78bbc
yamasaki <shohei615615@gmail.com>
2025-01-07 21:31:13 +0900

    選択するタブごとで表示する内容を切り替える処理を追加

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const searchTab = document.getElementById("js-search-tab");
 const wishlistTab = document.getElementById("js-wishlist-tab");
 const doneTab = document.getElementById("js-done-tab");
+const searchSection = document.getElementById("js-search-section");
+const wishlistSection = document.getElementById("js-wishlist-section");
+const doneSection = document.getElementById("js-done-section");
 
-wishlistTab.addEventListener("click", function () {
-  searchTab.classList.toggle("active");
-  wishlistTab.classList.toggle("active");
-});
-searchTab.addEventListener("click", function () {
-  searchTab.classList.toggle("active");
-  wishlistTab.classList.toggle("active");
-});
-doneTab.addEventListener("click", function () {
-  doneTab.classList.toggle("active");
-  wishlistTab.classList.toggle("active");
+const tabs = [searchTab, wishlistTab, doneTab];
+const sections = [searchSection, wishlistSection, doneSection];
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // 他のタブの "active" クラスを削除
+    tabs.forEach((t) => t.classList.remove("active"));
+
+    // クリックされたタブに "active" クラスを付与
+    tab.classList.add("active");
+
+    // クリックされたタブに対応するセクションを表示
+    const tabId = tab.id.replace("js-", "").replace("-tab", "");
+    sections.forEach((section) => {
+      section.classList.remove("active");
+    });
+    let sectionElement = document.getElementById(`js-${tabId}-section`);
+    sectionElement.classList.add("active");
+    console.log(sectionElement);
+  });
 });
